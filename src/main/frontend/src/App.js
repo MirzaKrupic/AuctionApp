06f19cdd745395eebfc3ace83569a7f497723b4c
Mirza <krupicmirza99@gmail.com>
2021-10-20 22:15:27 +0200

    add main navbar

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Navigation from './components/Navigation';
 
 function App() {
   return (
     <div className="App">
+      <Navigation />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Button>Test Button</Button>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/main/frontend/src/components/Navigation.js
@@ -0,0 +1,33 @@
+import { Navbar, Nav, Container, Col,  Row } from 'react-bootstrap';
+import React from 'react';
+import classes from './Navigation.module.css'
+import SearchBar from "material-ui-search-bar";
+
+function Navigation(){
+    return(
+    <Navbar className={classes.nav} collapseOnSelect fixed='top' expand='sm' variant='light'>
+        <Container className={"no-gutters mx-0 px-0"}>
+        <Row noGutters={true} className={classes.rowfit}>
+            <Col>
+        <Navbar.Brand className={classes.branding} href="/">Auction</Navbar.Brand>
+        </Col>
+        <Col xs={6}>
+        <SearchBar className={classes.bars} />
+        </Col>
+        <Col>
+            <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+            <Navbar.Collapse id='responsive-navbar-nav' className="justify-content-end">
+                <Nav>
+                    <Nav.Link className={classes.navchild} href='/'>Home</Nav.Link>
+                    <Nav.Link className={classes.navchild} href='/'>Shop</Nav.Link>
+                    <Nav.Link className={classes.navchild} href='/'>My Account</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Col>
+        </Row>
+        </Container>
+    </Navbar>
+    );
+}
+
+export default Navigation;
