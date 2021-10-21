f27d9ca724df4635e91726ba3dcb33fd436e294d
Mirza <krupicmirza99@gmail.com>
2021-10-21 12:19:35 +0200

    adding rest of footer

--- src/main/frontend/src/components/Footer.js
+++ src/main/frontend/src/components/Footer.js
@@ -1,48 +1,82 @@
-import classes from './Footer.module.css';
-import { Navbar, Nav, Container, Col,  Row } from 'react-bootstrap';
+import classes from "./Footer.module.css";
+import {
+  Container,
+  Col,
+  Row,
+  Form,
+  Button,
+} from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-    faGooglePlus,
-    faFacebook,
-    faTwitter,
-    faInstagram
-  } from "@fortawesome/free-brands-svg-icons";
+  faGooglePlus,
+  faFacebook,
+  faTwitter,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
 
-function Footer(){
-    return(
+function Footer() {
+  return (
     <footer>
-        <Container>
-            <Row className={classes.rowfit}>
-                <Col className={classes.coltest}>
-                    <h3>Auction</h3>
-                    <ul>
-                        <li>About us</li>
-                        <li>Terms and Conditions</li>
-                        <li>Privacy and Policy</li>
-                    </ul>
-                </Col>
-                    
-                <Col className={classes.coltest}>
-                <h3>Get in touch</h3>
-                    <ul>
-                        <li>Call Us at +123 797-567-2535</li>
-                        <li>support@auction.com</li>
-                        <li>
-                            <ul className={classes.socialmedia}>
-                                <li><FontAwesomeIcon icon={faFacebook} size="2x" /></li>
-                                <li><FontAwesomeIcon icon={faInstagram} size="2x" /></li>
-                                <li><FontAwesomeIcon icon={faTwitter} size="2x" /></li>
-                                <li><FontAwesomeIcon icon={faGooglePlus} size="2x" /></li>
-                            </ul>
-                        </li>
-                    </ul>
-                </Col>
-                <Col>
-                </Col>
-            </Row>
-        </Container>
+      <Container>
+        <Row className={classes.rowfit}>
+          <Col xs={12} md={4} className={classes.colfit}>
+            <h3>Auction</h3>
+            <ul>
+              <li>About us</li>
+              <li>Terms and Conditions</li>
+              <li>Privacy and Policy</li>
+            </ul>
+          </Col>
+
+          <Col xs={12} md={4} className={classes.colfit}>
+            <h3>Auction</h3>
+            <ul>
+              <li>Call Us at +123 797-567-2535</li>
+              <li>support@auction.com</li>
+              <li>
+                <ul className={classes.socialmedia}>
+                  <li>
+                    <FontAwesomeIcon icon={faFacebook} size="2x" />
+                  </li>
+                  <li>
+                    <FontAwesomeIcon icon={faInstagram} size="2x" />
+                  </li>
+                  <li>
+                    <FontAwesomeIcon icon={faTwitter} size="2x" />
+                  </li>
+                  <li>
+                    <FontAwesomeIcon icon={faGooglePlus} size="2x" />
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </Col>
+          <Col xs={12} md={4} className={classes.colfit}>
+            <h3>Newsletter</h3>
+            <ul>
+              <li>
+                Enter your email address and get notified about new products. We
+                hate spam!
+              </li>
+              <li>
+                <Form>
+                  <Form.Row className="d-flex align-items-end">
+                    <Form.Group as={Col} controlId="formGridEmail">
+                      <Form.Control className={classes.newsletterinput} type="email" placeholder="Your Email address" />
+                    </Form.Group>
+
+                    <Form.Group as={Col} controlId="formButton">
+                        <Button className={classes.newsletterbtn} variant="outline-secondary">Go</Button>
+                    </Form.Group>
+                  </Form.Row>
+                </Form>
+              </li>
+            </ul>
+          </Col>
+        </Row>
+      </Container>
     </footer>
-    );
+  );
 }
 
-export default Footer
+export default Footer;
