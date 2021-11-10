e4e0e0a826d755383994d7e072dbac45c9a3641f
Mirza <krupicmirza99@gmail.com>
2021-11-10 16:23:59 +0100

    minor itemService change

--- src/main/frontend/src/utils/itemService.js
+++ src/main/frontend/src/utils/itemService.js
@@ -1,15 +1,11 @@
 const host = "localhost:8080";
 
 export const itemsFetch = async (page, size) => {
-  const items = await (
-    await fetch(`http://${host}/api/v1/items?page=${page}&size=${size}`)
-  ).json();
-  return items;
+  const items = await fetch(`http://${host}/api/v1/items?page=${page}&size=${size}`)
+  return items.json();
 };
 
 export const itemsFetchByDate = async (page, size, order, orderColumn) => {
-  const items = await (
-    await fetch(`http://${host}/api/v1/items?page=${page}&size=${size}&order=${order}&orderColumn=${orderColumn}`)
-  ).json();
-  return items;
+  const items = await fetch(`http://${host}/api/v1/items?page=${page}&size=${size}&order=${order}&orderColumn=${orderColumn}`)
+  return items.json();
 };
