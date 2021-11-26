96230f7b9096a70a6d368134f4534615de93fc39
Mirza <krupicmirza99@gmail.com>
2021-11-26 17:59:04 +0100

    time left computing

--- src/main/frontend/src/components/itempage/ItemInfo.js
+++ src/main/frontend/src/components/itempage/ItemInfo.js
@@ -12,14 +12,55 @@ function ItemInfo(props) {
   const [response, setResponse] = useState();
   const [currentAmount, setCurrentAmount] = useState(0);
   const [currentNumberOfBids, setCurrentNumberOfBids] = useState(0);
+  const [timeLeft, setTimeLeft] = useState(0);
   const history = useHistory();
 
   useEffect(async () => {
     if(props.bids){
       setCurrentNumberOfBids(props.bids.length);
       setCurrentAmount(getHighestBid());
+
+      let endDate = props.auctionEndDate.slice(0, 10).split('-');
+      let endTime = props.auctionEndDate.slice(11).split(':');
+      endDate = new Date(endDate[0], endDate[1] - 1, endDate[2], endTime[0], endTime[1], endTime[2]); 
+
+      setTimeLeft(computeTimeLeft(endDate));
     }
   }, [props.bids]);
+
+  function computeTimeLeft(date) {
+    let d1 = date;
+    let d2 = new Date();
+    let now = new Date();
+    if (d2.getTime() < d1.getTime()) {
+      d1 = now;
+      d2 = date;
+    }
+    let yd = d1.getYear();
+    let yn = d2.getYear();
+    let years = yn - yd;
+    let md = d1.getMonth();
+    let mn = d2.getMonth();
+    let months = mn - md;
+    if (months < 0) {
+      years--;
+      months = 12 - md + mn;
+    }
+    let dd = d1.getDate();
+    let dn = d2.getDate();
+    let days = dn - dd;
+    if (days < 0) {
+      months--;
+      d2.setMonth(mn, 0);
+      days = d2.getDate() - dd + dn;
+    }
+    let weeks = Math.floor(days / 7);
+    days = days % 7;
+    if (years > 0) return years + ' years' + (months > 0 ? ' and ' + months + ' months' : ' and ' + weeks + ' weeks');
+    if (months > 0) return months + ' months' + (weeks > 0 ? ' and ' + weeks + ' weeks' : ' and ' + days + ' days');
+    if (weeks > 0) return weeks + ' weeks' + (days > 0 ? ' and ' + days + ' days' : '');
+    return days + ' days';
+ }
 
   const handleSubmit = async (item) => {
     const { amount } = item;
@@ -60,7 +101,7 @@ function ItemInfo(props) {
           </p>
           <p>
             {"Time left: "}
-            <span className={classes.detail_value}>{props.auctionEndDate}</span>
+            <span className={classes.detail_value}>{timeLeft}</span>
           </p>
         </div>
         <div className={classes.bidding_form_container}>
