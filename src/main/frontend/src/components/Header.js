dd7122dc034a9f36137d7e529fc7ba881502e78b
Mirza <krupicmirza99@gmail.com>
2021-11-28 11:22:39 +0100

    add logout feature

--- src/main/frontend/src/components/Header.js
+++ src/main/frontend/src/components/Header.js
@@ -4,8 +4,35 @@ import classes from "./Header.module.css";
 import { Link } from "react-router-dom";
 import SocialMedia from "./SocialMedia";
 import LayoutContainer from "./LayoutContainer";
+import { AuthContext } from "../hooks";
+import { useEffect, useState, useContext } from "react";
 
 function Header() {
+  const { token, isUserLoggedIn, setToken } = useContext(AuthContext);
+  const [rightContent, setRightContent] = useState(
+    <p className={classes.login_register_section}>
+      <Link to="/login">Login</Link> or
+      <Link to="/registration">Register</Link>
+    </p>
+  );
+
+  useEffect(async () => {
+    if (token !== null) {
+      setRightContent(
+        <p className={classes.logout} onClick={() => setToken(null)}>
+          Logout
+        </p>
+      );
+    }else{
+      setRightContent(
+        <p className={classes.login_register_section}>
+          <Link to="/login">Login</Link> or
+          <Link to="/registration">Register</Link>
+        </p>
+      )
+    }
+  }, [token]);
+
   return (
     <div className={classes.headercontainer}>
       <LayoutContainer>
@@ -13,12 +40,7 @@ function Header() {
           <Col className={classes.socialmediafit}>
             <SocialMedia />
           </Col>
-          <Col className={classes.login_register_position}>
-            <p className={classes.login_register_section}>
-              <Link to="/login">Login</Link> or
-              <Link to="/registration">Register</Link>
-            </p>
-          </Col>
+          <Col className={classes.login_register_position}>{rightContent}</Col>
         </Row>
       </LayoutContainer>
     </div>
