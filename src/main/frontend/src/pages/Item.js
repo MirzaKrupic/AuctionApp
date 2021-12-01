aeb68d8c1575ea2180aac668cf7cbcdf7d2287d7
Mirza <krupicmirza99@gmail.com>
2021-12-01 11:40:25 +0100

    indentation fix

--- src/main/frontend/src/pages/Item.js
+++ src/main/frontend/src/pages/Item.js
@@ -14,7 +14,7 @@ function Item() {
   const { token, setToken, isUserLoggedIn } = useContext(AuthContext);
 
   useEffect(async () => {
-    const fetchedItem = token ? await fetchItemById(itemId,token) : {};
+    const fetchedItem = token ? await fetchItemById(itemId, token) : {};
 
     setItem(fetchedItem);
   }, [token]);
@@ -23,9 +23,7 @@ function Item() {
     <LayoutContainer>
       <div className={classes.items_positioning}>
         <ImageGallery image={item.photo} />
-        <ItemInfo
-          {...item}
-        />
+        <ItemInfo {...item} />
       </div>
     </LayoutContainer>
   );
