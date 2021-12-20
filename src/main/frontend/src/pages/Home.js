e1b92775e9e87217e215ca020959589ea2037618
Mirza <krupicmirza99@gmail.com>
2021-12-20 08:46:55 +0100

    removing unfinished parts for demo

--- src/main/frontend/src/pages/Home.js
+++ src/main/frontend/src/pages/Home.js
@@ -8,50 +8,12 @@ import classes from "./Home.module.css";
 import LayoutContainer from "../components/LayoutContainer";
 
 function Home() {
-  const dummyCategories = [
-    {
-      id: 1,
-      name: "Fashion",
-    },
-    {
-      id: 2,
-      name: "Accessories",
-    },
-    {
-      id: 3,
-      name: "Jewelry",
-    },
-    {
-      id: 4,
-      name: "Shoes",
-    },
-    {
-      id: 5,
-      name: "Sportware",
-    },
-    {
-      id: 6,
-      name: "Home",
-    },
-    {
-      id: 7,
-      name: "Electronics",
-    },
-    {
-      id: 8,
-      name: "Mobile",
-    },
-    {
-      id: 9,
-      name: "Computer",
-    },
-  ];
 
   return (
     <LayoutContainer >
       <div className={classes.mainsection}>
-        <Categories categories={dummyCategories} />
-        <MainItemCard
+        <Categories />
+        {/* <MainItemCard
           description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
           hendrerit odio a erat lobortis auctor. Curabitur sodales pharetra
           placerat. Aenean auctor luctus tempus. Cras laoreet et magna in
@@ -59,12 +21,12 @@ function Home() {
           hac habitasse platea dictumst"
           price = "69,99"
           title = "Title"
-        />
+        /> */}
       </div>
 
-      <UpperSection />
+      {/* <UpperSection />
 
-      <MiddleSection />
+      <MiddleSection /> */}
 
       <LowerSection />
       </LayoutContainer>
