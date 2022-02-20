8734222d9d8f439d3e7d314c80859c4887deac29
Mirza Krupic <krupicmirza99@gmail.com>
2022-02-20 11:33:19 +0100

    redirecting if user is not logged in to login page

--- src/main/frontend/src/pages/My_Profile.js
+++ src/main/frontend/src/pages/My_Profile.js
@@ -1,7 +1,20 @@
 import { PAGES } from "../utils/constants";
+import { AuthContext } from "../hooks";
+import { useContext, useEffect, useState } from "react";
+import browserHistory from "history/createBrowserHistory";
 
 function My_Profile({ setCurrentPage }) {
   setCurrentPage(PAGES.MY_ACCOUNT);
+  const { token, setToken, isUserLoggedIn } = useContext(AuthContext);
+
+  useEffect(async () => {
+    console.log(token);
+    const history = browserHistory();
+    if (token === null) {
+      history.push("/login");
+      window.location.reload(false);
+    }
+  }, [token]);
 
   return <h1>my profile</h1>;
 }
