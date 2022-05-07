8ee4866076271a7527979b8ed6f1a4d4f54719b4
Mirza Krupic <krupicmirza99@gmail.com>
2022-05-07 16:13:31 +0200

    additiona info in form

--- src/main/frontend/src/pages/My_Profile.js
+++ src/main/frontend/src/pages/My_Profile.js
@@ -5,6 +5,8 @@ import classes from "./My_Profile.module.css";
 import browserHistory from "history/createBrowserHistory";
 import LayoutContainer from "../components/LayoutContainer";
 import { Button } from "react-bootstrap";
+import * as yup from "yup";
+import { Formik, Form, Field } from "formik";
 
 function My_Profile({ setCurrentPage }) {
   setCurrentPage(PAGES.MY_ACCOUNT);
@@ -45,7 +47,57 @@ function My_Profile({ setCurrentPage }) {
                   PLACE BID
                 </Button>
             </div>
-            <div className={classes.info_section}></div>
+            <div className={classes.info_section}>
+            <Formik
+          initialValues={{
+            firstName: "",
+            lastName: "",
+            email: "",
+            password: "",
+          }}
+        >
+          {({ errors, touched }) => (
+            <Form>
+              <label className={classes.input_container}>
+                First Name
+                <Field
+                  name="firstName"
+                  type="text"
+                  className={classes.registration_input}
+                />
+                {errors.firstName && touched.firstName ? (
+                  <div>{errors.firstName}</div>
+                ) : null}
+              </label>
+              <label className={classes.input_container}>
+                Last Name
+                <Field
+                  name="lastName"
+                  type="text"
+                  className={classes.registration_input}
+                />
+                {errors.lastName && touched.lastName ? (
+                  <div>{errors.lastName}</div>
+                ) : null}
+              </label>
+              <label className={classes.input_container}>
+                Email
+                <Field
+                  name="email"
+                  type="email"
+                  className={classes.registration_input}
+                />
+                {errors.email && touched.email ? (
+                  <div>{errors.email}</div>
+                ) : null}
+              </label>
+              <button className={classes.registration_button} type="submit">
+                Submit
+              </button>
+            </Form>
+          )}
+        </Formik>
+            </div>
           </div>
         </div>
       </LayoutContainer>
