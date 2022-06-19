7a4958a606938843da64cb75910a5f0d0ef4a0ac
Mirza Krupic <krupicmirza99@gmail.com>
2022-06-19 16:45:04 +0200

    websocket testing

---
+++ src/main/java/com/example/auctionapp/socketinjo/GreetingController.java
@@ -0,0 +1,27 @@
+package com.example.auctionapp.socketinjo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.util.HtmlUtils;
+
+@Controller
+public class GreetingController {
+
+    @Autowired
+    private SimpMessagingTemplate simpMessagingTemplate;
+    private static final String WS_MESSAGE_TRANSFER_DESTINATION = "/topic/greetings/1";
+
+    @MessageMapping("/hello")
+    public void greet(HelloMessage message) throws InterruptedException {
+        Thread.sleep(2000);
+        simpMessagingTemplate.convertAndSend(WS_MESSAGE_TRANSFER_DESTINATION,
+                new Greeting("Hello, " +
+                        HtmlUtils.htmlEscape(message.getName())));
+
+    }
+}
